e43159cc461c1412f237aba300e2e447ab0b25ca
mtbshnikkk <iartuom@mail.ru>
2022-01-31 19:31:29 +0500

    Глупый бот [#31323]

--- src/test/java/ru/job4j/condition/DummyBot.java
+++ src/test/java/ru/job4j/condition/DummyBot.java
@@ -1,13 +1,11 @@
 package ru.job4j.condition;
 
 public class DummyBot {
-    @SuppressWarnings("checkstyle:RightCurly")
     public static String answer(String question) {
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         if ("Привет, Бот.".equals(question)) {
             rsl = "Привет, умник.";
-        }
-        if ("Пока.".equals(question)) {
+        } else if ("Пока.".equals(question)) {
             rsl = "До скорой встречи.";
         }
         return rsl;
