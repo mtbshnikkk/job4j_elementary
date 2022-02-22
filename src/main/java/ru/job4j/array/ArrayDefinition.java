b8bad7c7361d1f13e59348242240ad4cb9479957
mtbshnikkk <iartuom@mail.ru>
2022-02-22 21:46:27 +0500

    Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -4,9 +4,21 @@ public class ArrayDefinition {
     public static void main(String[] args) {
         short[] age = new short[10];
         System.out.println("Размер массива равен: " + age.length);
+
         String[] surnames = new String[100500];
         System.out.println("Размер массива равен " + surnames.length);
+
         float[] prices = new float[40];
         System.out.println("Размер массива равен " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Лалалэй";
+        names[1] = "Котофей";
+        names[2] = "Молофей";
+        names[3] = "Бармалей";
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+        System.out.println(names[0]);
+        System.out.println(names[1]);
     }
 }
