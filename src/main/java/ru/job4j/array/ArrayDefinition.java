6fc7c850a4107c08fb71d27b0d5a9bf347b218e2
mtbshnikkk <iartuom@mail.ru>
2022-02-22 21:30:33 +0500

    Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,7 +3,10 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] age = new short[10];
+        System.out.println("Размер массива равен: " + age.length);
         String[] surnames = new String[100500];
+        System.out.println("Размер массива равен " + surnames.length);
         float[] prices = new float[40];
+        System.out.println("Размер массива равен " + prices.length);
     }
 }
